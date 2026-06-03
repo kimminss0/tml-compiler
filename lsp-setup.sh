3244210fd05fc4b83ec389c814fe1ff325390cb1
Minseo Kim <kimminss0@outlook.kr>
2026-06-03 12:22:36 +0900

    chore: remove old build scripts

--- lsp-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-eval $(opam env --switch=4.14.2 --set-switch)
-mv hw7 hw7.bak
-make clean
-cp typing_stub.ml typing.ml
-make
-mv hw7.bak hw7
-echo "LSP setup: compiled with ocamlc $(ocamlc --version)"
